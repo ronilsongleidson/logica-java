224505439207ec0a76a817a72da45eaae075772d
Entra21 <entra21@proway.treina>
2021-05-11 21:19:17 -0300

    aprendendo conversao

---
+++ src/CalculaAreaDoTrapezio.java
@@ -0,0 +1,16 @@
+public class CalculaAreaDoTrapezio {
+
+    public static void main(String[] args) {
+
+        double b, h, B, area;
+        float valor = 1.0f;
+        h = 5.0;
+        b = 6.2;
+        B = 8.0;
+
+        area = (b + B) / 2.0 * h;
+
+
+        System.out.println(area);
+    }
+}
