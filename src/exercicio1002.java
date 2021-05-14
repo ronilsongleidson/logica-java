67325cab350fcfc070895006f7ca865d164729bb
Ronilson Xavier <ronilson.gleidson@gmail.com>
2021-05-13 22:36:24 -0300

    calculando a area de um circulo (esqueci de commitar na aula)

---
+++ src/exercicio1002.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double n = 3.14159;
+        double raio = scanner.nextDouble();
+        double area;
+
+
+        area = n * (raio * raio);
+
+        System.out.printf("A=%.4f", area);
+
+    }
+}
