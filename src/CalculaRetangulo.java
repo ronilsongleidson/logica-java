47b435796f069a5b9439e1aae42c383ef731a884
Entra21 <entra21@proway.treina>
2021-05-20 20:27:21 -0300

    revisando resolvendo exercicio em sala AREA DO RETANGULO

---
+++ src/CalculaRetangulo.java
@@ -0,0 +1,35 @@
+/*
+Fazer um programa para ler as medidas da base e altura de um retângulo.
+Em seguida,  mostrar o valor da área, perímetro e diagonal deste retângulo,
+com quatro casas  decimais, conforme exemplo.
+
+Exemplo:
+ Entrada:       Saída:
+5.0             AREA = 20.0000
+4.0             PERIMETRO = 18.0000
+                DIAGONAL = 6.4031
+
+ */
+import java.util.Scanner;
+
+public class CalculaRetangulo {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double base = 5.0;
+        double altura = 4.0;
+
+
+        double area = base * altura;
+        double perimetro = 2 * base + 2 * altura;
+        double diagonal = Math.sqrt(base * base + altura * altura);
+
+        System.out.println("Area = " + area);
+        System.out.println("PERIMETRO = " + perimetro);
+        System.out.println("Diagonal = " + diagonal);
+
+        scanner.close();
+
+    }
+}
