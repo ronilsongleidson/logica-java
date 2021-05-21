0ba7dc1104845a2e7725725fd4622de820fb3003
Entra21 <entra21@proway.treina>
2021-05-20 21:30:28 -0300

    acrescentando funcoes de matematica

--- src/CalculaRetangulo.java
+++ src/CalculaRetangulo.java
@@ -26,7 +26,7 @@ public class CalculaRetangulo {
 
         double area = base * altura;
         double perimetro = 2 * base + 2 * altura;
-        double diagonal = Math.sqrt(base * base + altura * altura);
+        double diagonal = Math.sqrt(Math.pow(base,2) + Math.pow(altura,2));
 
         System.out.printf(" Area = %.4f%n", area);
         System.out.printf("Perimetro = %.4f%n",perimetro);
