33c9191696b696b373426794ae442474f3a8c527
Your Name <you@example.com>
2021-06-11 20:28:01 -0300

    incluindo o enunciando

--- src/estruturarepeticao/enquanto/CalculaMediaEnquanto.java
+++ src/estruturarepeticao/enquanto/CalculaMediaEnquanto.java
@@ -1,4 +1,16 @@
 package estruturarepeticao.enquanto;
+/**
+ * Faça um programa para ler um número indeterminado de dados, contendo cada um, a
+ * idade de um indivíduo. O último dado, que não entrará nos cálculos, contém um valor
+ * de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos. Se for
+ * entrado um valor negativo na primeira vez, mostrar a mensagem "impossivel calcular".
+ * <p>
+ * Entrada          Saida
+ * 31               34.67
+ * 27
+ * 46
+ * -5
+ */
 
 import java.util.Locale;
 import java.util.Scanner;
