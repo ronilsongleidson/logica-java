f57f3bf9529fe5dfa4d7e8ed993fcee4aa39e95b
Entra21 <entra21@proway.treina>
2021-06-18 19:33:41 -0300

    usando do while -

--- src/estruturarepeticao/para/Uri1067NumerosImpares.java
+++ src/estruturarepeticao/para/Uri1067NumerosImpares.java
@@ -9,7 +9,7 @@ public class Uri1067NumerosImpares {
         int x = scanner.nextInt();
         if (x>= 1 && x<1000 ) {
             for (int i = 1; i <= x; i++) {
-                if (i % 2 != 0) {
+                if (i % 2 != 0) {           // usando modulo para converter em impar
                     System.out.println(i);
                 }
             }
